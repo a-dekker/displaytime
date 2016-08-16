486418fe3342c7535bf5fd0c84a313a3d6e91ede
Ancelad <i_am_pomidorchik@mail.ru>
2016-08-16 12:19:15 +0500

    Add Russian translation

---
+++ translations/harbour-displaytime-ru.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>SettingPage</name>
+    <message>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <source>Keep backlight on</source>
+        <translation>Поддерживать подсветку включенной</translation>
+    </message>
+    <message>
+        <source>Lock clock to landscape</source>
+        <translation>Заблокировать часы в ландшафте</translation>
+    </message>
+    <message>
+        <source>Custom display color</source>
+        <translation>Настроить цвет экрана</translation>
+    </message>
+    <message>
+        <source>Pick Color</source>
+        <translation>Выбор цвета</translation>
+    </message>
+    <message>
+        <source>Clock-text brightness</source>
+        <translation>Яркость текста часов</translation>
+    </message>
+</context>
+</TS>
