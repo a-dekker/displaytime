5f35b0c43afbdf52956f237d70ae392981ed57be
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-09-16 23:00:05 +0800

    Create harbour-displaytime-zh_CN.ts

---
+++ translations/harbour-displaytime-zh_CN.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>SettingPage</name>
+    <message>
+        <source>Save</source>
+        <translation>保存</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <source>Keep backlight on</source>
+        <translation>保持背光开启</translation>
+    </message>
+    <message>
+        <source>Lock clock to landscape</source>
+        <translation>锁屏时钟显示为横屏模式</translation>
+    </message>
+    <message>
+        <source>Custom display color</source>
+        <translation>自定义显示颜色</translation>
+    </message>
+    <message>
+        <source>Pick Color</source>
+        <translation>选择颜色</translation>
+    </message>
+    <message>
+        <source>Clock-text brightness</source>
+        <translation>时钟文本亮度</translation>
+    </message>
+</context>
+</TS>
